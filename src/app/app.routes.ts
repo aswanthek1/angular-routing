152337844c971c3471255ed5547fd159f9c05730
Aswanth Raveendran E K <aswanthek1@gmail.com>
2024-01-04 10:23:22 +0530

    Tested canActivate method of guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,15 +9,17 @@ import { authGuard } from './guards/auth-guard.guard';
 import { ObservablesComponent } from './observables/observables.component';
 import { UnsubscribeComponent } from './unsubscribe/unsubscribe.component';
 import { LoginComponent } from './login/login.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 export const routes: Routes = [
     {path:'', title:'Home', component:FirstComponent},
     {path:'second', title:'Second' ,component:SecondComponent},
     {path:'contact', title:'Contact', component: ContactComponent},
-    {path:'products', title:'Products', component: ProductslistComponent, canActivate:[authGuard]},
+    {path:'products', title:'Products', component: ProductslistComponent},
     // {path:'products/:id', title:'Products', component: ProductsdetailsComponent, canActivate:[authGuard]},
     {path: 'products', children: [
-        {path:':id', component:ProductsdetailsComponent}
+        {path:':id', component:ProductsdetailsComponent},
+        {path:'product/checkout', component:CheckoutComponent, canActivate:[authGuard]}
     ]},
     {path:'obs', title:'Obs', component: ObservablesComponent},
     {path:'obs/unsubscribe', title:'unsub', component: UnsubscribeComponent},
@@ -25,5 +27,5 @@ export const routes: Routes = [
     {path:'login', component:LoginComponent},
 
     {path:'not-found', title:'Not Found', component:NotFoundComponent},
-    {path:'**', component:NotFoundComponent}
+    // {path:'**', component:NotFoundComponent}
 ];
